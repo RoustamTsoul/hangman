c342cd7b52a64e57a2a3da88e810cbe49486e6b6
Roustam Tsoulfas <roustamtsoulfas@gmail.com>
2022-07-19 19:15:26 +0200

    addimage

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
+// inputs:
+// -background city-id(hint)
+// -one of img from 0 to 6 for hangman state (head, legs, arms)
 
-let word = capitals[Math.floor(Math.random()*capitals.length)];
-console.log(word);
+// outputs:
+// -picture of city on background wall
+// -picture of one of the seven states(head, arm, leg)
 
-let answerArray = [];
-for (let i=0; i<=word.length; i++){
-    answerArray[i]="_";
-    
-}
-        
-let remainingLetters = word.length;
-let attempt = 7;
-     
-
-while(remainingLetters > 0) {
-    // Основной код
-    // Показываем состояние игры
-    alert(answerArray.join(" "));
-    // Запрашиваем вариант ответа
-    let guess = prompt ("Угадайте букву или нажмите Отмена для выхода из игры.");
-    let guessLower = guess.toLowerCase();
-    // Обновляем answerArray и remainingLetters для каждого
-    // вхождения угаданной буквы
-
-    if (guess === null){
-        break;
-    } else if (guess.length !== 1) {
-        alert ("Пожалуйста, введите только одну букву.");
-    } else { 
-        let hit = false;
-        for (let j = 0; j < word.length; j++) {
-            let letterWasBeen = answerArray[j] === guessLower;
-            if (letterWasBeen){
-                alert("Вы уже вводили эту букву");
-                break;
-            } 
-            if (word[j] === guessLower) {
-                hit = true;
-                answerArray[j] = guessLower;
-                remainingLetters--; 
-                
-            } 
-        } 
-
-        if (!hit) {
-            attempt--;
-            alert("У Вас осталось "  + attempt + " попыток");  
-        }         
-    } 
-
-    if (attempt <= 0) {
-        alert ("Ваши попытки закончились");
-        break;
-    }
-}
-if (attempt > 0){
-    alert(answerArray.join(" "));
-    alert("Отлично! Было загадано слово " + word); 
-}
+// 2/3 width of display
+// 100% height of display
